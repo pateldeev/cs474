c794fb9412497593e9e311ba4bd51b56b8817396
pateldeev <pateldeev@nevada.unr.edu>
2018-11-27 17:10:39 -0800

    fixed centering

--- src/PA04/Experiment4.cpp
+++ src/PA04/Experiment4.cpp
@@ -11,104 +11,111 @@
 
 int main(int argc, char * argv[]) {
 
-	const std::string imgFile = "images/PA04/Experiment4/girl.pgm";
-	//----------------------------------------------------------------------------
-	// Start reading image in
-	int M, N, Q;
-	bool type;
-	readImageHeader(imgFile.c_str(), N, M, Q, type);
-	ImageType og_image(N, M, Q);
-	readImage(imgFile.c_str(), og_image);
-	//----------------------------------------------------------------------------
+    const std::string imgFile = "images/PA04/Experiment4/girl.pgm";
+    const std::string exp_image_real = "images/PA04/Experiment4/homomorphic_girl.pgm";
+    //----------------------------------------------------------------------------
+    // Start reading image in
+    int M, N, Q;
+    bool type;
+    readImageHeader(imgFile.c_str(), N, M, Q, type);
+    ImageType og_image(N, M, Q);
+    readImage(imgFile.c_str(), og_image);
+    //----------------------------------------------------------------------------
 
-	// Create imaginary part of image
-	ImageType og_imaginary(N, M, Q);
-	for(int i = 0; i < M; ++i){
-		for(int j = 0; j < N; j++){
-			og_imaginary.setPixelVal(i, j, 0);
-		}
-	}
-	// Use ImageComplex for simplicity since
-	// logarithmic functions will work on it
-	ImageComplex ln_image(og_image, og_imaginary);
+    // Create imaginary part of image - zero by default
+    ImageType og_imaginary(N, M, Q);
 
-	// Natural log the original image
-	float tempR, tempI;
-	for(int i = 0; i < M; ++i){
-		for(int j = 0; j < N; j++){
-			ln_image.getPixelVal(i, j, tempR, tempI);
-			ln_image.setPixelVal(i, j, log(tempR), tempI);
-		}
-	}
+    // Use ImageComplex for simplicity since
+    // logarithmic functions will work on it
+    ImageComplex ln_image(og_image, og_imaginary);
 
-	// Forward Fourier Transform
-	ln_image.applyFFT(true);
+    // Natural log the original image
+    float tempR, tempI;
+    for (int i = 0; i < M; ++i)
+        for (int j = 0; j < N; j++) {
+            ln_image.getPixelVal(i, j, tempR, tempI);
+            ln_image.setPixelVal(i, j, std::log(tempR), tempI);
+        }
 
-	// High Pass Filter
-	float y_low = 0.5, y_high = 1.5, d_cutoff = 1.8;
-	for(int i = 0; i < M; ++i){
-		for(int j = 0; j < N; j++){
-			ln_image.getPixelVal(i, j, tempR, tempI);
-			tempR = 1 - exp(-(tempR * tempR + tempI * tempI) / (d_cutoff * d_cutoff));
-			tempR *= (y_high - y_low);
-			tempR += y_low;
-			ln_image.setPixelVal(i, j, tempR, tempI);
-		}
-	}
+    // Forward Fourier Transform
+    ln_image.applyFFT(true);
 
-	// Inverse Fourier Transform
-	ln_image.applyFFT(false);
+    //display spectrum
+    ImageType imgSpec(N, M, Q);
+    ln_image.getSpectrum(imgSpec);
+    writeImage("images/PA04/Experiment4/ln_spectrum.pgm", imgSpec);
 
-	// Apply exponential function
-	ImageType modified_real(N, M, Q), modified_imaginary(N, M, Q);
-	ImageComplex exp_image(modified_real, modified_imaginary);
-	for(int i = 0; i < M; ++i){
-		for(int j = 0; j < N; j++){
-			ln_image.getPixelVal(i, j, tempR, tempI);
-			exp_image.setPixelVal(i, j, exp(tempR), tempI);
-		}
-	}
+    // High Pass Filter
+    const float y_low = 0.5, y_high = 1.5, d_cutoff = 1.8, c = 1;
+    ImageComplex filter(N, M);
+    for (int i = 0; i < M; ++i)
+        for (int j = 0; j < N; j++) {
+            int u = j - M / 2, v = N / 2 - i; //u and v coordinates of pixel
+            tempR = 1 - std::exp(-c * ((u * u + v * v) / (d_cutoff * d_cutoff)));
+            tempR *= (y_high - y_low);
+            tempR += y_low;
+            filter.setPixelVal(i, j, tempR, 0);
+        }
 
-	// Change type back to ImageType
-	exp_image.getImageType(modified_real, modified_imaginary, false);
+    //multiply by filter
+    ln_image.complexMultiplication(filter);
 
-	const std::string exp_image_real = "images/PA04/Experiment4/homomorphic_girl.pgm";
-	writeImage(exp_image_real.c_str(), modified_real);
+    //display spectrum
+    ln_image.getSpectrum(imgSpec);
+    writeImage("images/PA04/Experiment4/filtered_spectrum.pgm", imgSpec);
 
-	return 0;
+
+    // Inverse Fourier Transform
+    ln_image.applyFFT(false);
+
+    // Apply exponential function
+    ImageType modified_real(N, M, Q), modified_imaginary(N, M, Q);
+    ImageComplex exp_image(N, M);
+    for (int i = 0; i < M; ++i)
+        for (int j = 0; j < N; j++) {
+            ln_image.getPixelVal(i, j, tempR, tempI);
+            exp_image.setPixelVal(i, j, std::exp(tempR), tempI);
+        }
+
+    // Change type back to ImageType
+    exp_image.getImageType(modified_real, modified_imaginary);
+
+    writeImage(exp_image_real.c_str(), modified_real);
+
+    return 0;
 }
 
 
 
 /* Useless Code
 
-	// Normalize
-	float minVal, maxVal, tempVal, tempVal_I;
-	ln_image.getPixelVal(0, 0, minVal, tempVal_I);
-	maxVal = minVal;
-	for(int i = 0; i < M; ++i){
-		for(int j = 0; j < N; j++){
-			ln_image.getPixelVal(i, j, tempVal, tempVal_I);
-			if(tempVal < minVal){
-				minVal = tempVal;
-			}
-			if(tempVal > maxVal){
-				maxVal = tempVal;
-			}
-		}
-	}
-	std::cout << "Max value: " << maxVal << "\nMin value: " << minVal << std::endl;
+        // Normalize
+        float minVal, maxVal, tempVal, tempVal_I;
+        ln_image.getPixelVal(0, 0, minVal, tempVal_I);
+        maxVal = minVal;
+        for(int i = 0; i < M; ++i){
+                for(int j = 0; j < N; j++){
+                        ln_image.getPixelVal(i, j, tempVal, tempVal_I);
+                        if(tempVal < minVal){
+                                minVal = tempVal;
+                        }
+                        if(tempVal > maxVal){
+                                maxVal = tempVal;
+                        }
+                }
+        }
+        std::cout << "Max value: " << maxVal << "\nMin value: " << minVal << std::endl;
 
-	for(int i = 0; i < M; ++i){
-		for(int j = 0; j < N; j++){
-			ln_image.getPixelVal(i, j, tempVal, tempVal_I);
-			tempVal = ((tempVal - minVal) / (maxVal - minVal) * log(255));
-			ln_image.setPixelVal(i, j, tempVal, tempVal_I);
-			if(i < 3 && j < 3){
-				std::cout << tempVal << " ";
-			}
-		}
-	}
-	std::cout << std::endl;
+        for(int i = 0; i < M; ++i){
+                for(int j = 0; j < N; j++){
+                        ln_image.getPixelVal(i, j, tempVal, tempVal_I);
+                        tempVal = ((tempVal - minVal) / (maxVal - minVal) * log(255));
+                        ln_image.setPixelVal(i, j, tempVal, tempVal_I);
+                        if(i < 3 && j < 3){
+                                std::cout << tempVal << " ";
+                        }
+                }
+        }
+        std::cout << std::endl;
 
-*/
+ */
